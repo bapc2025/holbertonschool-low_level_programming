01050fc0f5b8ac5fd6e4757c9a7e88d7c916d0af
bapc2025 <3603@holbertonschool.com>
2021-10-05 18:51:51 +0000

    Nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times,
+ * in lowercase, followed by a new line.
+ *
+ * Return: Always .
+ */
+void print_alphabet_x10(void);
+int main(void)
+{
+	print_alphabet_x10(void);
+	return (0);
+}
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
