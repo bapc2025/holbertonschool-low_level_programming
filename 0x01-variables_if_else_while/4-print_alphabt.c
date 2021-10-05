21678dcb0c3fe42a0a95788110a39d5e826e2b81
bapc2025 <3603@holbertonschool.com>
2021-10-05 02:53:54 +0000

    C

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -12,8 +12,10 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		if (letter != 'e' && letter != 'q')
+			putchar(letter);
 	}
-	putchar(letter);
+
+	putchar('\n');
 
 	return (0);
 }
