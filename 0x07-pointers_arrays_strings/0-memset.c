1d989bfead61298c37047d9ba2bbd2cfeb8b17a9
bapc2025 <3603@holbertonschool.com>
2021-10-19 18:57:32 +0000

    Nested loops

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,16 +1,13 @@
 #include "main.h"
 
-/*
- * _memset - fill the first n bytes of memory
- * pointed to by @s with a contant byte @b
+/**
+ * _memset - fill array s with b n number of times
  * @s: a pointer of the memory area to be filled
  * @b: character to fill the memory area
- * @n the number of bytes to be filled
- *
+ * @n: the number of bytes to be filled
  * Return: a pointer to the filled memory area @s
  */
 char *_memset(char *s, char b, unsigned int n)
-
 {
 	unsigned int i;
 
