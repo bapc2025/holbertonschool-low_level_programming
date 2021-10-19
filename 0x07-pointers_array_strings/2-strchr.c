63c651c1c27092d5f3886acca7007ce5aacfcef8
bapc2025 <3603@holbertonschool.com>
2021-10-19 18:25:55 +0000

    Nested loops

--- 0x07-pointers_array_strings/2-strchr.c
+++ 0x07-pointers_array_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /*
  * *_strchr - locate a character in a string.
  * @s - string to search.
@@ -8,5 +9,20 @@
  * c in the string s of NULL if the character
  * is not found.
  */
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
 {
+
+	int a;
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
