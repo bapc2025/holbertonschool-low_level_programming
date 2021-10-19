7905e3f838086fc22a9fd22bb0cbd6cd21a51359
bapc2025 <3603@holbertonschool.com>
2021-10-19 16:54:15 +0000

    Nested loops

---
+++ 0x07-pointers_array_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * memset - fill the first n bytes of memory
+ * pointed to by @s with a contant byte @b
+ * @s: a pointer of the memory area to be filled
+ * @b: character to fill the memory area
+ * @n the number of bytes to be filled
+ *
+ * Return: a pointer to the filled memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return(s);
+}
---
+++ 0x07-pointers_array_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/*
+ * *_strchr - locate a character in a string.
+ * @s - string to search.
+ * @c - character to find
+ *
+ * Return: pointer to first ocurrence of ther character
+ * c in the string s of NULL if the character
+ * is not found.
+ */
+char *_strchr(char *s, char c);
+{
