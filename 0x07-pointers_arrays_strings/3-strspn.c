ebcf333681f5440de6afab57ff64b9fbbd905cdd
bapc2025 <3603@holbertonschool.com>
2021-10-20 02:16:43 +0000

    Nested loops

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,13 +1,11 @@
 #include "main.h"
-
 /**
- * _strspn - gets the length of a prefix substring
- * @s: string to evaluate
- * @accept: string containing the list of characters to match in s
- *
- * Retunrn: the number of bytes in the initial segment
- * of s wich consist only of bytes from accept
- */
+ * _strspn - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept in s
+ * Return: bytes required to find accept
+ **/
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, exit;
