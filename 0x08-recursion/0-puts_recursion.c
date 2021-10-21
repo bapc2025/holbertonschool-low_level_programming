1f04c5a56ff9c608150b5844a67c8a94af1e79bb
bapc2025 <3603@holbertonschool.com>
2021-10-21 13:30:39 +0000

    Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints string recursion
+ * @s: string 
+ * Return: nothing
+ **/
+void _puts_recursion(char *s)
+{
+	int i;
+	i = 0;
+	of (s[i] != '\0')
+	{
+		_putschar(s[i]);
+		_puts_recursion(&s[i + 1]);
+	}
+	else
+		_putchar('\n')
+}
