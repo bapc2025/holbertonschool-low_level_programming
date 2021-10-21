ef539a568676c1b2087dc25a237dd64adcbbf150
bapc2025 <3603@holbertonschool.com>
2021-10-21 16:28:36 +0000

    Recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+
+/**
+ * factorial - return factorial number of n
+ * @n: int number find factorial number
+ * Return: factorial number of n
+ */
+int factorial(int n)
+{
+if (n < 0)
+return (-1);
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
+}
