8b130988ca6f356b3b8ce3ae8171bc1d942198fc
bapc2025 <3603@holbertonschool.com>
2021-10-21 13:48:44 +0000

    Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,18 @@
  * @s: string 
  * Return: nothing
  **/
+
 void _puts_recursion(char *s)
 {
 	int i;
+
 	i = 0;
-	if (s[i] != '\0');
+
+	if (s[i] != '\0')
 	{
-i		_putschar(s[i]);
+		_putchar(s[i]);
 		_puts_recursion(&s[i + 1]);
 	}
 	else
-		_putchar('\n')
+		_putchar('\n');
 }
