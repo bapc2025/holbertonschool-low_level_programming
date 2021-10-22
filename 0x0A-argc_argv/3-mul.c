dba7d15af755f1483035b3d75cdb0093ce2e743d
bapc2025 <3603@holbertonschool.com>
2021-10-22 23:00:25 +0000

    Nested loops

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,23 +1,30 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - multiply by two numbers
- * @argc: arguments count
- * @argv:array of pointers
- *
- * Return: result of multiplication or 1
+ * main - multiplies 2 integers in the terminal
+ * @argc: argument counts
+ * @argv: argument array
+ * Return: returns 1 if no digit is added or if 3 + digits are added.
+ * (Error) and 0 if suceeded
  */
+
+
 int main(int argc, char *argv[])
+
 {
-	int i;
-	int product = 1;
-	if (argc <= 2)
+	int number1, number2, result;
+
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 1; i < argc; i++)
-		product = product * _atoi(argv[i]);
-	printf("%i\n", product);
+
+	number1 = atoi(argv[1]);
+	number2 = atoi(argv[2]);
+	result = (number1 * number2);
+	printf("%d\n", result);
 	return (0);
 }
