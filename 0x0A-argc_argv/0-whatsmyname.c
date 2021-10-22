1cf51840a5cfc284c148f37309417c5a8b97a75c
bapc2025 <3603@holbertonschool.com>
2021-10-22 16:14:43 +0000

    argc and argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 /**
  * main - prints program name folloes by a new line.
- * @argc - number of arguments.
- * @argv - array pointer to the argument.
+ * @argc: number of arguments.
+ * @argv: array pointer to the argument.
+ *
+ * Return: 0
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv [0]);
-	return 0;
+	printf("%s\n", argv[0]);
+	return (0);
 }
