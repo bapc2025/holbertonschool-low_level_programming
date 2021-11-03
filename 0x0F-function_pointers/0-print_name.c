3a5553c007def48f9d6814f5034aeb0a2f114a07
bapc2025 <3603@holbertonschool.com>
2021-11-03 23:32:29 +0000

    please work

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: ponter to function
+ * print_name: prints name
+ * @name: name to be printed
+ * @f: pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
