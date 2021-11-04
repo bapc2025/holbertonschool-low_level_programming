c912cec4ddb6e1b951d68cd66e515c158010acc2
bapc2025 <3603@holbertonschool.com>
2021-11-04 00:05:53 +0000

    function pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-
 /**
   * print_name - Prints a string using a pointer
   * @name: name string.
@@ -7,6 +6,9 @@
   */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-		f(name);
+	if (f == NULL)
+		return;
+	if (name == NULL)
+		return;
+	f(name);
 }
