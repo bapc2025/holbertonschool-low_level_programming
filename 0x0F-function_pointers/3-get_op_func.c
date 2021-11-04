d740fcaea478fb3ce80079f42d7883636b1f65fa
bapc2025 <3603@holbertonschool.com>
2021-11-04 02:22:45 +0000

    function pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,73 +2,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * op_add - sum two ints
- * @a: int
- * @b: int
- * Return: sum
+ * get_op_func - function that selects the correct function
+ * @s: pointer to char
+ * Return: operator
  */
-int op_add(int a, int b)
+int (*get_op_func(char *s))(int, int)
 {
-	int sum;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int a = 0;
 
-	sum = (a + b);
-	return (sum);
-}
-/**
- * op_sub - difference of two ints
- * @a: int
- * @b: int
- * Return: sub
- */
-int op_sub(int a, int b)
-{
-	int sub;
-
-	return (sub = (a - b));
-}
-/**
- * op_mul - product of two ints
- * @a: int
- * @b: int
- * Return: mul
- */
-int op_mul(int a, int b)
-{
-	int mul;
-
-	return (mul = (a * b));
-}
-/**
- * op_div - divide two ints
- * @a: int
- * @b: int
- * Return: div
- */
-int op_div(int a, int b)
-{
-	int div;
-
-	if (b == 0)
+	while (a < 5)
 	{
-		printf("Error\n");
-		exit(100);
+		if (ops[a].op[0] == s[0] && !s[1])
+			return (ops[a].f);
+		a++;
 	}
-	return (div = (a / b));
-}
-/**
- * op_mod - remainder of div
- * @a: int
- * @b: int
- * Return: mod
- */
-int op_mod(int a, int b)
-{
-	int mod;
-
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return (mod = (a % b));
+	printf("Error\n");
+	exit(99);
 }
